418c857bd7d0314623585ef082f75f8afc76dfde
Sreinumder <rojgosai02@gmail.com>
2024-10-02 10:00:16 +0545

    fun

---
+++ script.js
@@ -0,0 +1,25 @@
+function userInput(input) {
+  console.log(input);
+  cpu_input = Math.ceil(Math.random()*3);
+  console.log(cpu_input);
+  const cpuImg = document.getElementById('CPU_img');
+  cpuImg.src = `${cpu_input}.jpg`;
+  const userImg = document.getElementById('user_img');
+  userImg.src = `${input}.jpg`;
+  let result;
+  if (input === cpu_input){
+    console.log("Draw");
+    result = "Draw";
+  }
+  else if (input === (cpu_input +1) % 3 ){
+    console.log("Win");
+    result = "Win";
+  }
+  else { 
+    console.log("Lose");
+    result = "Lose";
+
+  }
+  const outcome = document.getElementById('result');
+  outcome.innerText = result;
+}
